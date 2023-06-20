0d2741258c19bf615b0d658a2eacd17f423cb986
serap erkan <serapperkan@gmail.com>
2023-06-20 19:26:14 +0300

    .

---
+++ js/index.js
@@ -0,0 +1,510 @@
+
+let products = [
+
+    {
+        _id: 1,
+        type: "dog",
+        size: "25cm",
+        price: 200,
+        icon: "./images/dog/dog8f.png",
+        img: "./images/dog/dog8.png",
+        msg: "Peluş Köpek-dog"
+    },
+    {
+        _id: 2,
+        type: "bird",
+        size: "25cm",
+        price: 200,
+        icon: "./images/bird/21.png",
+        img: "./images/bird/21.png",
+        msg: "Peluş Kuş-bird"
+    },
+    {
+        _id: 3,
+        type: "cat",
+        size: "25cm",
+        price: 200,
+        icon: "./images/cat/28.png",
+        img: "./images/cat/29.png",
+        msg: "Peluş Kedi-cat"
+    },
+
+    {
+        _id: 4,
+        type: "dog",
+        size: "25cm",
+        price: 200,
+        icon: "./images/dog/dog1f.png",
+        img: "./images/dog/dog1.png",
+        msg: "Peluş Köpek-dog"
+    },
+    {
+        _id: 5,
+        type: "bird",
+        size: "25cm",
+        price: 200,
+        icon: "./images/bird/19.png",
+        img: "./images/bird/19.png",
+        msg: "Peluş Kuş-bird"
+    },
+
+
+
+    {
+        _id: 6,
+        type: "dog",
+        size: "25cm",
+        price: 200,
+        icon: "./images/dog/dog2f.png",
+        img: "./images/dog/dog2.png",
+        msg: "Peluş Köpek-dog"
+    },
+
+
+
+    {
+        _id: 7,
+        type: "cat",
+        size: "25cm",
+        price: 200,
+        icon: "./images/cat/30.png",
+        img: "./images/cat/31.png",
+        msg: "Peluş Kedi-cat"
+    },
+    {
+
+        _id: 8,
+        type: "cat",
+        size: "25cm",
+        price: 200,
+        icon: "./images/cat/32.png",
+        img: "./images/cat/33.png",
+        msg: "Peluş Kedi-cat"
+    },
+
+    {
+        _id: 9,
+        type: "bird",
+        size: "25cm",
+        price: 200,
+        icon: "./images/bird/20.png",
+        img: "./images/bird/20.png",
+        msg: "Peluş Kuş-bird"
+    },
+
+
+    {
+        _id: 10,
+        type: "dog",
+        size: "25cm",
+        price: 200,
+        icon: "./images/dog/dog3f.png",
+        img: "./images/dog/dog3.png",
+        msg: "Peluş Köpek-dog"
+    },
+    {
+        _id: 11,
+        type: "bird",
+        size: "25cm",
+        price: 200,
+        icon: "./images/bird/18.png",
+        img: "./images/bird/18.png",
+        msg: "Peluş Kuş-bird"
+    },
+    {
+        _id: 12,
+        type: "dog",
+        size: "25cm",
+        price: 200,
+        icon: "./images/dog/dog4f.png",
+        img: "./images/dog/dog4.png",
+        msg: "Peluş Köpek-dog"
+    },
+    {
+        _id: 13,
+        type: "bird",
+        size: "25cm",
+        price: 200,
+        icon: "./images/bird/15.png",
+        img: "./images/bird/15.png",
+        msg: "Peluş Kuş-bird"
+    },
+
+
+
+    {
+        _id: 14,
+        type: "cat",
+        size: "25cm",
+        price: 200,
+        icon: "./images/cat/34.png",
+        img: "./images/cat/35.png",
+        msg: "Peluş Kedi-cat"
+    },
+    {
+        _id: 15,
+        type: "bird",
+        size: "25cm",
+        price: 200,
+        icon: "./images/bird/17.png",
+        img: "./images/bird/17.png",
+        msg: "Peluş Kuş-bird"
+    },
+    {
+        _id: 16,
+        type: "bird",
+        size: "25cm",
+        price: 200,
+        icon: "./images/bird/14.png",
+        img: "./images/bird/14.png",
+        msg: "Peluş Kuş-bird"
+    },
+
+
+
+    {
+        _id: 17,
+        type: "cat",
+        size: "25cm",
+        price: 200,
+        icon: "./images/cat/36.png",
+        img: "./images/cat/37.png",
+        msg: "Peluş Kedi-cat"
+    },
+
+
+    {
+        _id: 18,
+        type: "bird",
+        size: "25cm",
+        price: 200,
+        icon: "./images/bird/13.png",
+        img: "./images/bird/13.png",
+        msg: "Peluş Kuş-bird"
+    },
+
+
+    {
+        _id: 19,
+        type: "bird",
+        size: "25cm",
+        price: 200,
+        icon: "./images/bird/12.png",
+        img: "./images/bird/12.png",
+        msg: "Peluş Kuş-bird"
+    },
+
+
+
+    {
+        _id: 20,
+        type: "dog",
+        size: "25cm",
+        price: 200,
+        icon: "./images/dog/dog5f.png",
+        img: "./images/dog/dog5.png",
+        msg: "Peluş Köpek-dog"
+    },
+    {
+        _id: 21,
+        type: "bird",
+        size: "25cm",
+        price: 200,
+        icon: "./images/bird/16.png",
+        img: "./images/bird/16.png",
+        msg: "Peluş Kuş-bird"
+    },
+
+
+
+    {
+
+        _id: 22,
+        type: "cat",
+        size: "25cm",
+        price: 200,
+        icon: "./images/cat/38.png",
+        img: "./images/cat/39.png",
+        msg: "Peluş Kedi-cat"
+    },
+    {
+        _id: 23,
+        type: "bird",
+        size: "25cm",
+        price: 200,
+        icon: "./images/bird/10.png",
+        img: "./images/bird/10.png",
+        msg: "Peluş Kuş-bird"
+    },
+
+
+    {
+        _id: 24,
+        type: "cat",
+        size: "25cm",
+        price: 200,
+        icon: "./images/cat/41.png",
+        img: "./images/cat/40.png",
+        msg: "Peluş Kedi-cat"
+    },
+    {
+        _id: 25,
+        type: "bird",
+        size: "25cm",
+        price: 200,
+        icon: "./images/bird/9.png",
+        img: "./images/bird/9.png",
+        msg: "Peluş Kuş-bird"
+    },
+
+
+
+    {
+        _id: 26,
+
+        type: "cat",
+        size: "25cm",
+        price: 200,
+        icon: "./images/cat/42.png",
+        img: "./images/cat/43.png",
+        msg: "Peluş Kedi-cat"
+    },
+    {
+        _id: 27,
+        type: "bird",
+        size: "25cm",
+        price: 200,
+        icon: "./images/bird/8.png",
+        img: "./images/bird/8.png",
+        msg: "Peluş Kuş-bird"
+    },
+
+
+    {
+        _id: 28,
+        type: "dog",
+        size: "25cm",
+        price: 200,
+        icon: "./images/dog/dog6f.png",
+        img: "./images/dog/dog6.png",
+        msg: "Peluş Köpek-dog"
+    },
+    {
+        _id: 29,
+        type: "bird",
+        size: "25cm",
+        price: 200,
+        icon: "./images/bird/7.png",
+        img: "./images/bird/7.png",
+        msg: "Peluş Kuş-bird"
+    },
+
+
+    {
+        _id: 30,
+        type: "cat",
+        size: "25cm",
+        price: 200,
+        icon: "./images/cat/44.png",
+        img: "./images/cat/45.png",
+        msg: "Peluş Kedi-cat"
+    },
+
+
+    {
+        _id: 31,
+        type: "bird",
+        size: "25cm",
+        price: 200,
+        icon: "./images/bird/6.png",
+        img: "./images/bird/6.png",
+        msg: "Peluş Kuş-bird"
+    },
+
+    {
+        _id: 32,
+        type: "dog",
+        size: "25cm",
+        price: 200,
+        icon: "./images/dog/dog7f.png",
+        img: "./images/dog/dog7.png",
+        msg: "Peluş Köpek-dog"
+    },
+
+
+
+    {
+        _id: 33,
+        type: "bird",
+        size: "25cm",
+        price: 200,
+        icon: "./images/bird/5.png",
+        img: "./images/bird/5.png",
+        msg: "Peluş Kuş-bird"
+    },
+
+
+
+]
+
+let navbar = document.querySelector('.navbar');
+// navbar scroll
+window.addEventListener('scroll', function () {
+    let position = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    if (position >= 80) {
+        navbar.classList.add('bg-navbar');
+    } else {
+        navbar.classList.remove('bg-navbar');
+    }
+});
+
+let index = 0;
+let total = 0;
+let cardId = document.getElementById("cardId");
+let searchInput = document.getElementById("search");
+let cardSpan = document.getElementById("cardSpan");
+
+let dogId = document.getElementById("dog");
+let birdId = document.getElementById("bird");
+let fishId = document.getElementById("fish");
+let catId = document.getElementById("cat");
+
+
+getProduct(products);
+
+
+
+searchInput.addEventListener('keyup', search);
+dogId.addEventListener('click', filter);
+birdId.addEventListener('click', filter);
+fishId.addEventListener('click', filter);
+catId.addEventListener('click', filter);
+
+
+
+function filter(e) {
+    let type = e.target.parentElement.id;
+    console.log("type ", type);
+    const filtered = products.filter(item => item.type == type);
+    getProduct(filtered);
+    console.log("filtered", filtered, filtered[0]._id);
+
+}
+
+
+function getProduct(products) {
+    cardId.innerHTML = "";
+    for (let p of products) {
+
+        cardId.innerHTML += `
+        <div class="col mt-5 mb-5 d-flex justify-content-center " >
+    
+        <div class="cardDiv d-flex flex-column align-items-center  ">
+          <div class="over">
+            <i class="fa-solid fa-basket-shopping fa-xl ps-4 pt-5"  style="color: #fcfcff;"></i>
+            <img src="${p.icon}" class="iconCard">
+            <p><a class="btn btn-outline-warning addBtn" href="#" id="${p._id}"> sepete ekle »</a></p>
+            <p class="cartTitle mb-4"> Sepete eklemeyi unutma ..!</p>
+          </div>
+          <div class="cardImgDiv mb-5 mt-5">
+            <img src="${p.img}" class="cardImg mt-5" alt="Placeholder" />
+          </div>
+          Price: ${p.price}
+          -Size:${p.size}
+          <h2 class="fw-5 ">${p.msg} </h2>
+        </div>
+      </div>`
+
+    }
+
+    let addBtnClass = document.querySelectorAll(".addBtn");
+    for (let element of addBtnClass) {
+        element.addEventListener("click", addBtnFunction);
+    }
+
+}
+
+
+function search(e) {
+    let newProducts = products.filter(p => p.msg.toLowerCase().includes(e.target.value.toLowerCase()));
+    getProduct(newProducts);
+
+}
+
+
+function addBtnFunction(e) {
+    index++;
+    cardSpan.style.display = "block";
+    cardSpan.innerHTML = index;
+    payment(e.target.id);
+
+}
+
+function payment(productId) {
+    let cardDropMenu = document.getElementById("cardDropMenu");
+    let selectedProduct = products.filter(item => item._id == productId);
+
+
+    if (selectedProduct) {
+
+        // console.log("Eklenecek ürün ID:", selectedProduct[0]._id);
+        // console.log("Eklenecek ürün type:", selectedProduct[0].type);
+        // console.log("Eklenecek ürün fiyatı:", selectedProduct[0].price);
+        // console.log("Eklenecek ürün boyut:", selectedProduct[0].size);
+        // console.log("Eklenecek ürün icon:", selectedProduct[0].icon);
+
+
+        //ilk eklenen çocuk dispalay :none sepet doldu 
+        let firstChild = cardDropMenu.firstElementChild;
+        firstChild.style.display = "none";
+
+        //total idye sahip  elementi silip yeniden oluşturalım total değeri yenilensin
+
+        cardDropMenu.innerHTML += `
+        <li><a class="dropdown-item" href="#">
+        <div class="d-flex  align-items-center">
+          <img src="${selectedProduct[0].icon}" class="rounded-circle basketIcon me-4 " /> 
+          <div class="ms-3 ">
+          <p>${selectedProduct[0].msg}</p>
+            <p>type:${selectedProduct[0].type}</p>
+            <p>price: ${selectedProduct[0].price}</p>
+            <p>size:${selectedProduct[0].size}</p>
+          </div>
+          <span id="${selectedProduct[0]._id}" class="close" >x</span>
+        </div>
+        </a></li>
+        `
+    }
+
+    let totalEl = document.getElementById("total");
+    console.log(totalEl);
+    if (totalEl) {
+        totalEl.remove();
+    }
+
+    total = total + parseInt(selectedProduct[0].price);
+    console.log("total", total);
+    cardDropMenu.innerHTML += `
+     <li class="total" id="total"><a class=" text-center " href="#">Toplam:${total}</a></li>`
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
